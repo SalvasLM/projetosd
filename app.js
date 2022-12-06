88f8932873552db67f4ba2bebcd3c70e57c46a9e
Luis Silva <ifaf.silva12@gmail.com>
2022-12-06 16:22:36 +0000

    c

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-var express = require('express');
-var path = require('path');
-const bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
+const app = require('./express/app');
+const sequelize = require('./sequelize');
+const cors = require('cors')
+const PORT = 8080;
 
-app.use(bodyParser.json())
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-)
+async function assertDatabaseConnectionOk() {
+    console.log(`Checking database connection...`);
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection OK!');
+    } catch (error) {
+        console.log('Unable to connect to the database:');
+        console.log(error.message);
+        process.exit(1);
+    }
+}
 
-const users = require('./routes/users');
-const files = require('./routes/files');
+async function init() {
+    await assertDatabaseConnectionOk();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    console.log(`Starting Sequelize + Express example on port ${PORT}...`);
 
+    const corsOpts = { origin: '*', methods: ['GET', 'POST', 'DELETE', 'PUT'], allowedHeaders: ['Content-Type']};
+    app.use(cors(corsOpts));
 
-app.get('/api/users', users.getUsers);
-app.get('/api/files', files.getFiles);
+    app.listen(PORT, () => {
+        console.log(`Express server started on port ${PORT}. Try some routes, such as '/api/users'.`);
+    });
+}
 
-app.get('/api/users/:id(\\d+)', users.getUserById);
-app.get('/api/files/:id(\\d+)', files.getFileById);
-
-app.post('/api/users', users.createUser);
-app.post('/api/files', files.createFile);
-
-app.delete('/api/users/:id(\\d+)', users.deleteUser);
-app.delete('/api/files/:id(\\d+)', files.deleteFile);
-
-app.put('/api/users', users.updateUser);
-app.put('/api/files', files.updateFile);
-
-app.post('/api/login', users.getUserLogin);
-
-module.exports = app;
+init();
---
+++ sequelize/index.js
@@ -0,0 +1,65 @@
+const { Sequelize } = require('sequelize');
+const { applyExtraSetup } = require('../../projetosdAPI/sequelize/extra-setup');
+const connectionString = process.env.DATABASE_URL
+const connectionHost = process.env.DATABASE_HOST
+const connectionPW = process.env.DATABASE_PASSWORD
+const connectionUser = process.env.DATABASE_USER
+const connectionPort = process.env.DATABASE_PORT
+const connectionDatabase = process.env.DATABASE_DB
+
+
+
+
+/*const sequelize = new Sequelize(connectionUser, connectionDatabase, connectionPW, {
+    host: connectionHost,
+    port: connectionPort,
+    dialect: 'postgres',
+    logQueryParameters: true,
+    benchmark: true,
+    define: {
+        timestamps: false
+    }
+
+});*/
+
+
+
+const sequelize = new Sequelize('postgres', 'postgres', '7CnaH1Gghs0J32L', {
+    host: 'sdprojectdb.internal',
+    port: connectionPort,
+    dialect: 'postgres',
+    logQueryParameters: true,
+    benchmark: true,
+    define: {
+        timestamps: false
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+const modelDefiners = [
+    users = require('../../projetosdAPI/sequelize/models/users'),
+    files = require('../../projetosdAPI/sequelize/models/files'),
+
+    // Add more models here...
+    // require('./models/item'),
+];
+
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+    modelDefiner(sequelize, Sequelize);
+}
+
+// We execute any extra setup after the models are defined, such as adding associations.
+applyExtraSetup(sequelize);
+
+// We export the sequelize connection instance to be used around our app.
+module.exports = sequelize;
